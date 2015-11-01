6cebeb0d19b0c073c98f7fc45c8287c954b66c8a
dali <dali@ufofactory.org>
2015-11-02 08:58:07 +0900

    이슈 리스트 화면 다듬기

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,4 +3,6 @@ class Issue < ActiveRecord::Base
   has_many :statuses
   has_many :comments
   belongs_to :user
+
+  default_scope { order(id: :desc) }
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,6 @@
 class Status < ActiveRecord::Base
   belongs_to :source, polymorphic: true
+  default_scope { order(id: :desc) }
 
   def contents
     source.contents_as_status
