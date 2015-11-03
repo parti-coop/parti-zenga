1d39b3bb1b7784d63e61bf55f3d7b8339424162d
dali <dali@ufofactory.org>
2015-11-03 19:41:48 +0900

    커맨트 리펙토링

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,21 @@
 class CommentsController < ApplicationController
-  include Commenting
-  before_action :prepare_commenting, only: :create
+  before_action :authenticate_user!
+  before_action :set_issue
+  before_action :set_proposition
 
   def create
-    create_comment
+    @comment = @issue.comments.new(create_params)
+    @comment.user = current_user
+    @comment.proposition = @proposition
+    unless @comment.save
+      flash[:error] = t('application.error')
+    end
+
     redirect_to @issue
   end
 
   def new
-    @comment = issue.comments.new
-    @proposition = Proposition.find params[:proposition_id] if params[:proposition_id].present?
+    @comment = @issue.comments.new
     respond_to do |format|
       format.js
     end
@@ -17,7 +23,16 @@ class CommentsController < ApplicationController
 
   private
 
-  def issue
-    Issue.find params[:issue_id]
+  def set_issue
+    @issue = Issue.find params[:issue_id]
+  end
+
+  def set_proposition
+    proposition_id = params[:comment].present? ? params[:comment][:proposition_id] : params[:proposition_id]
+    @proposition = Proposition.find proposition_id if proposition_id.present?
+  end
+
+  def create_params
+    params.require(:comment).permit([:contents])
   end
 end
