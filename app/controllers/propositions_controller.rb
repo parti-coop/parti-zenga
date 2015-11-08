3c32fc582facfea533be5aac60ea23bdf3e40377
dali <dali@ufofactory.org>
2015-11-08 18:19:02 +0900

    이슈와 상태 화면 다듬기

--- app/controllers/propositions_controller.rb
+++ app/controllers/propositions_controller.rb
@@ -1,5 +1,5 @@
 class PropositionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :issue, only: [:new, :create]
 
   def show
